d3d66157a9d2b79f3ad7f0192163f939648cbd5e
HinrichsJadeHs <niklas.hinrichs@student.jade-hs.de>
2020-10-05 15:28:11 +0200

    nein

---
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahnradaufgabe_Gruppe_D_HSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kuhydöghd
+        }
+    }
+}
