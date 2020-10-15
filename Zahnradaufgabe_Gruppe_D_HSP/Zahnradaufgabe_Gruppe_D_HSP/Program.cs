567977e62d2b4076c66731a1111778e8b0bf75ae
HinrichsJadeHs <71779617+HinrichsJadeHs@users.noreply.github.com>
2020-10-15 10:13:25 +0200

    Anfang?

--- Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -7,12 +7,50 @@ using System.Threading.Tasks;
 namespace Zahnradaufgabe_Gruppe_D_HSP
 {
     class Program
+        
     {
+        double c = 0.167;
         static void Main(string[] args)
         {
-            Console.WriteLine("Moin in die Gruppe! und Ich bin Claude");
-            Console.WriteLine("aber wo ist xiaowei ?");   
+            Console.WriteLine("Ein herzerfrischendes Moin Moin an den Anwender!");
+            Console.WriteLine("Mit dieser Anwendung können Sie ein Zahnrad konfigurieren.");
+            Console.WriteLine("");
+            Console.WriteLine("Zur Berechnung werden folgende Daten benötigt: ");
+            Console.WriteLine("Modul, Zähnezahl, Teilkreisdurchmesser, Breite und das Material.");
+
+
+
+            Console.Write("Geben Sie das Modul ein: ");
+            double modul = Convert.ToDouble(Console.ReadLine());
+          
+
+            Console.Write("Geben Sie die Zähnezahl ein: ");
+            double Zähnezahl = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie den Teilkreisdurchmesser ein: ");
+            double Teilkreisdurchmesser = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geben Sie die Breite ein: ");
+            double Breite = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.WriteLine("Danke für ihre Eingabe. Mit ENTER wird berechnet!");
             Console.ReadKey();
+
+            Program prg = new Program();
+            double h = prg.Zahnhöhe_z(modul);
+            Console.WriteLine("Die Zahnhöhe h lautet: " + h);
+            Console.ReadKey();
+
+            Console.Write("Geben Sie das Material ein: ");
+            string Material = Convert.ToString(Console.ReadLine());
+            Console.ReadKey();
+        }
+        public double Zahnhöhe_z(double modul)
+        {
+           
+            double h = (2 * modul) + c;
+            return h;
         }
     }
 }
