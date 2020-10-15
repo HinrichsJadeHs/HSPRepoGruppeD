9d5f2b83a5b0c9f54e090b316dd5e6e2d6f543ae
HinrichsJadeHs <niklas.hinrichs@student.jade-hs.de>
2020-10-15 14:09:53 +0200

    2.

--- Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -10,7 +10,7 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
         
     {
         //Constante Variablen
-        const double c = 0.167;
+        const double Spielconstante = 0.167;
         const double PI = 3.1415926535897932384626433832;
         double cosAngle;
 
@@ -44,23 +44,32 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
             // Eingegebene Daten werden in Unterprogrammen berechnet und ausgegeben
             Program prg = new Program();
 
+            //Dezimalstellen angeben
+            int deci = 3;
+
             double modul = prg.Modul_m(teilkreisdurchmesser, zähnezahl);
-            Console.WriteLine("Das Modul m lautet: " + modul);
+            Console.WriteLine("Das Modul                        m   = " + Math.Round(modul , deci) + "mm");
 
             double h = prg.Zahnhöhe_h(modul);
-            Console.WriteLine("Die Zahnhöhe h lautet: " + h);
-            
+            Console.WriteLine("Die Zahnhöhe                     h   = " + Math.Round(h , deci) + "mm");
+
+            double hf = prg.Zahnfusshöhe_hf(modul);
+            Console.WriteLine("Die Zahnfusshöhe                 hf  = " + Math.Round(hf , deci) + "mm");
+
+            double ha = prg.Zahnkopfhöhe_ha(modul);
+            Console.WriteLine("Die Zahnkopfhöhe                 ha  = " + Math.Round(ha, deci) + "mm");
+
             double p = prg.Teilung_p(modul);
-            Console.WriteLine("Die Teilung p lautet: " + p);
+            Console.WriteLine("Die Teilung                      p   = " + Math.Round(p, deci) + "mm");
 
             double df = prg.Fusskreisdurchnmesser_df(modul, teilkreisdurchmesser);
-            Console.WriteLine("Der Fußkreisdurchnmesser df lautet: " + df);
+            Console.WriteLine("Der Fußkreisdurchnmesser         df  = " + Math.Round(df, deci) + "mm");
 
             double da = prg.Kopfkreisdurchnmesser_da(modul, zähnezahl);
-            Console.WriteLine("Der Kopfkreisdurchnmesser da lautet: " + da);
+            Console.WriteLine("Der Kopfkreisdurchnmesser        da  = " + Math.Round(da, deci) + "mm");
 
-            double db = prg.Grundkreisdurchnmesser_db(teilkreisdurchmesser);
-            Console.WriteLine("Der Grundkreisdurchnmesser db lautet: " + db);
+            double db = prg.Grundkreisdurchmesser_db(teilkreisdurchmesser);
+            Console.WriteLine("Der Grundkreisdurchnmesser       db  = " + Math.Round(db, deci) + "mm");
 
 
             Console.ReadKey();
@@ -70,6 +79,10 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
         }
 
         //Berechnungen
+        double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180.0;
+        }
         public double Modul_m(double teilkreisdurchmesser, double zähnezahl)
         {
             double modul = teilkreisdurchmesser / zähnezahl;
@@ -77,9 +90,23 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
         }
         public double Zahnhöhe_h(double modul)
         {
+            double c = Spielconstante * modul;
             double h = (2 * modul) + c;
-            double hf = modul + c;
             return h ;
+        }
+
+        public double Zahnfusshöhe_hf(double modul)
+        {
+            double c = Spielconstante * modul;
+            double hf = modul + c;
+            return hf;
+        }
+
+        public double Zahnkopfhöhe_ha(double modul)
+        {
+            
+            double ha = modul;
+            return ha;
         }
 
         public double Teilung_p(double modul)
@@ -89,6 +116,7 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
         }
         public double Fusskreisdurchnmesser_df(double modul, double teilkreisdurchmesser)
         {
+            double c = Spielconstante * modul;
             double df = teilkreisdurchmesser - (2 * (modul + c));
             return df;
         }
@@ -97,9 +125,9 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
             double da = modul * (zähnezahl - 2);
             return da;
         }
-        public double Grundkreisdurchnmesser_db(double teilkreisdurchmesser)
+        public double Grundkreisdurchmesser_db(double teilkreisdurchmesser)
         {
-            cosAngle = Math.Cos(20);
+            cosAngle = Math.Cos(DegreesToRadians(20));
             double db = teilkreisdurchmesser * cosAngle;
             return db;
         }
