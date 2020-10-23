fefb1b3c393a0f73c71a0a29373dabbb64903041
HinrichsJadeHs <niklas.hinrichs@student.jade-hs.de>
2020-10-23 09:42:29 +0200

    Update Program.cs

--- Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -18,20 +18,26 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
 
             //Begrüßung in einer Methode
             Begrüßung();
-            
-            
-            
+
+
+
             //Konsoleneingabe
-            
+            Console.Write("Zähnezahl: ");
+            double zähnezahl = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Teilkreisdurchmesser: ");
+            double teilkreisdurchmesser = Convert.ToDouble(Console.ReadLine());
 
             // Eingegebene Daten werden in Unterprogrammen berechnet und ausgegeben
 
-            
+
         }
 
         static void Begrüßung() //Methode Begrüßung 1. Hallo/Moin/.... 2. Wir berechnen ein Zahnrad
         {
-            Console.WriteLine("Xiaoweisdfsefsefsef");
+            Console.WriteLine("Hallo an den Anwender!");
+            Console.WriteLine("Willkommen bei dem Zahnradrechner");
+            Console.WriteLine("Bitte geben Sie Die Zähnezahl und den Teilkreisdurchmesser (in mm) an");
+            Console.WriteLine("Mit ENTER gehts weiter!");
             Console.ReadKey();
         }
         
