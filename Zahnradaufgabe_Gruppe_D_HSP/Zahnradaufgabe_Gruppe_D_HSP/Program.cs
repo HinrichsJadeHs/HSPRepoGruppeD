9575472b88e07cbfd0b34e64e8582d8479679eb0
HinrichsJadeHs <71779617+HinrichsJadeHs@users.noreply.github.com>
2020-10-25 13:24:11 +0100

    update Nik

--- Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -15,28 +15,39 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
 
         static void Main(string[] args)  //Hauptschleife
         {
-
             //Begrüßung in einer Methode
             Begrüßung();
-
+            //Begrüßung in einer Methode
 
 
             //Konsoleneingabe
             Console.Write("Zähnezahl: ");
-            double zähnezahl = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
             Console.Write("Teilkreisdurchmesser: ");
-            double teilkreisdurchmesser = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine(zähnezahl);
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.ReadKey();
+            //Konsoleneingabe
+
+
+            //Verarbeitung in einer Methode
+            Program prg = new Program();
+            double m = prg.Modul_m(d, z);
+            double c = prg.Kopfspiel_c(m);
+            double h = prg.Zahnhöhe_h(m, c);
+            //Verarbeitung in einer Methode
+
+
+            //Ausgabe
+            Console.WriteLine("Das Modul                        m   = " + Math.Round(m, 2) + "mm");           
+            Console.WriteLine("Die Zahnhöhe                     h   = " + Math.Round(h, 2) + "mm");
+            //Ausgabe
+
             Console.ReadKey();
 
-
-            // Modul = teilkreisdurchmesser / zähnezahl
             // Eingegebene Daten werden in Unterprogrammen berechnet und ausgegeben
-
-
         }
 
-        static void Begrüßung() //Methode Begrüßung 1. Hallo/Moin/.... 2. Wir berechnen ein Zahnrad
+        static void Begrüßung() //Methode Begrüßung 
         {
             Console.WriteLine("Hallo an den Anwender!");
             Console.WriteLine("Willkommen bei dem Zahnradrechner");
@@ -46,44 +57,67 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
         }
         
         //Berechnungen
-        
+        public double Modul_m(double d, double z)
+        {
+            double m = d / z;
+            return m;
+        }
+
+        public double Zahnhöhe_h(double m, double c)
+        {
+            double h = 2 * m + c;
+            return h;
+        }
+       //Berechnungen
+
+        //Konstanten
+        public double Kopfspiel_c(double m)
+        {
+            double c = m * 0.167;
+            return c;
+        }
+        //Konstanten
+
+
+        private static void Xiao(string[] args)//xiaowei
+        {
+            Console.WriteLine("Hallo an den Anwender!");
+            Console.WriteLine("Willkommen bei dem Zahnradrechner");
+            Console.WriteLine("Bitte geben Sie Die Zahnhöhe(z) und den Teilkreisdurchmesser（d) (in mm) an");
+            Console.WriteLine("Mit ENTER gehts weiter!");
+            Console.ReadKey();
+
+            Console.WriteLine("z: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("d: ");
+            double d = Convert.ToDouble(Console.ReadLine());
+
+            double m = d / z;
+            double c = 0.167 * m;
+            Console.ReadKey();
+
+            Console.WriteLine("Es stehen vier Ergebinisse zur Benutz");
+            double q = m + c;
+            Console.WriteLine("Zahnhöhe=" + q);
+
+
+            double e = m;
+            Console.WriteLine("Zahnkopfhöhe=" + m);
+
+            double s = m * 3.14;
+            Console.WriteLine("Teilung=" + s);
+
+            double h = d - 2 * (m + c);
+            Console.WriteLine("Fusskreisdurchmesser=" + h);
+
+            Console.ReadKey();
+
+
+            Console.WriteLine("Danke für das Benutzen");
+            Console.ReadKey();
+        }
+
 
     }
 }
-static void Main(string[] args)//xiaowei
-{
-    Console.WriteLine("Hallo an den Anwender!");
-    Console.WriteLine("Willkommen bei dem Zahnradrechner");
-    Console.WriteLine("Bitte geben Sie Die Zahnhöhe(z) und den Teilkreisdurchmesser（d) (in mm) an");
-    Console.WriteLine("Mit ENTER gehts weiter!");
-    Console.ReadKey();
 
-    Console.WriteLine("z: ");
-    double z = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("d: ");
-    double d = Convert.ToDouble(Console.ReadLine());
-
-    double m = d / z;
-    double c = 0.167 * m;
-    Console.ReadKey();
-
-    Console.WriteLine("Es stehen vier Ergebinisse zur Benutz");
-    double q = m + c;
-    Console.WriteLine("Zahnhöhe=" + q);
-
-
-    double e = m;
-    Console.WriteLine("Zahnkopfhöhe=" + m);
-
-    double s = m * 3.14;
-    Console.WriteLine("Teilung=" + s);
-
-    double h = d - 2 * (m + c);
-    Console.WriteLine("Fusskreisdurchmesser=" + h);
-
-    Console.ReadKey();
-
-
-    Console.WriteLine("Danke für das Benutzen");
-    Console.ReadKey();
-}
