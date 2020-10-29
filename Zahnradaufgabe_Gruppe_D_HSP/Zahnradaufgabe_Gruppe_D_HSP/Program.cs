dd14bb33e00583faffceac02be9820ca1188b86c
HinrichsJadeHs <71779617+HinrichsJadeHs@users.noreply.github.com>
2020-10-29 09:16:56 +0100

    Update Nik

--- Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
+++ Zahnradaufgabe_Gruppe_D_HSP/Zahnradaufgabe_Gruppe_D_HSP/Program.cs
@@ -25,6 +25,8 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
             double z = Convert.ToDouble(Console.ReadLine());
             Console.Write("Teilkreisdurchmesser: ");
             double d = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Geben sie die Anzahl der gerundeten Nachkommarstellen an: ");
+            int nachkommar = Convert.ToInt32(Console.ReadLine());
             Console.ReadKey();
             //Konsoleneingabe
 
@@ -34,12 +36,23 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
             double m = prg.Modul_m(d, z);
             double c = prg.Kopfspiel_c(m);
             double h = prg.Zahnhöhe_h(m, c);
+            double df = prg.Fusskreisdurchmesser_df(d, m, c);
+            double da = prg.Kopfkreisdurchmesser_da(m, z);
+            double p = prg.Teilung_p(m);
+            double hf = prg.Zahnfusshöhe_hf(m, c);
+            double ha = m;
             //Verarbeitung in einer Methode
 
 
+
             //Ausgabe
-            Console.WriteLine("Das Modul                        m   = " + Math.Round(m, 2) + "mm");           
-            Console.WriteLine("Die Zahnhöhe                     h   = " + Math.Round(h, 2) + "mm");
+            Console.WriteLine("Das Modul                        m   = " + Math.Round(m, nachkommar));
+            Console.WriteLine("Die Teilung                      p   = " + Math.Round(p, nachkommar));
+            Console.WriteLine("Die Zahnhöhe                     h   = " + Math.Round(h, nachkommar) + "mm");
+            Console.WriteLine("Der Fußkreisdurchmesser          df  = " + Math.Round(df, nachkommar) + "mm");
+            Console.WriteLine("Der Kopfkreisdurchmesser         da  = " + Math.Round(da, nachkommar) + "mm");
+            Console.WriteLine("Die Zahnfusshöhe                 hf  = " + Math.Round(hf, nachkommar) + "mm");
+            Console.WriteLine("Die Zahnkopfhöhe                 ha  = " + Math.Round(ha, nachkommar) + "mm");
             //Ausgabe
 
             Console.ReadKey();
@@ -68,7 +81,28 @@ namespace Zahnradaufgabe_Gruppe_D_HSP
             double h = 2 * m + c;
             return h;
         }
-       //Berechnungen
+
+        public double Fusskreisdurchmesser_df(double d, double m, double c)
+        {
+            double df = d - 2 * (m + c);
+            return df;
+        }
+        public double Kopfkreisdurchmesser_da(double m, double z)
+        {
+            double da = m * (z + 2);
+            return da;
+        }
+        public double Teilung_p(double m)
+        {
+            double p = m * Math.PI;
+            return p;
+        }
+        public double Zahnfusshöhe_hf(double m, double c)
+        {
+            double hf = m + c;
+            return hf;
+        }
+        //Berechnungen
 
         //Konstanten
         public double Kopfspiel_c(double m)
