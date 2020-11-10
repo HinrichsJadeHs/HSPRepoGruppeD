09efe111cafa93a873a835e39e7cb73195befe3f
HinrichsJadeHs <niklas.hinrichs@student.jade-hs.de>
2020-11-10 22:10:54 +0100

    8

--- WPFZahnradaufgabeGruppeD/WPFZahnradaufgabeGruppeD/MainWindow.xaml.cs
+++ WPFZahnradaufgabeGruppeD/WPFZahnradaufgabeGruppeD/MainWindow.xaml.cs
@@ -15,34 +15,75 @@ using System.Windows.Shapes;
 
 namespace WPFZahnradaufgabeGruppeD
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-
-
-
+      
         public MainWindow()
         {
-
             
             InitializeComponent();
-
-            
+                     
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
-            
-            int z = Convert.ToInt32( zähnezahl.Text);
-           
-            double m = Convert.ToDouble(DropModul.Text);         
-            double d = z * m;
-            ausgabe.Text = Convert.ToString(d);
+                  
+            double z = Convert.ToDouble( zähnezahl.Text);         
+            double m = Convert.ToDouble(DropModul.Text);
+
+
+
+            if (z % 1 == 0 && z >= 5 )
+            {
+                zähnezahl.Background = Brushes.White;
+
+                double d = m * z;
+                d_Ausgabe.Text = Convert.ToString(Math.Round(d, 2) + " mm");
+
+                double p = Math.PI * m;
+                p_Ausgabe.Text = Convert.ToString(Math.Round(p, 2));
+
+                double da = d + 2 * m;
+                da_Ausgabe.Text = Convert.ToString(Math.Round(da, 2) + " mm");
+
+                double c = 0.167;
+                c_Ausgabe.Text = Convert.ToString(Math.Round(c, 2) + " mm");
+
+                double df = d - 2 * (m + c);
+                df_Ausgabe.Text = Convert.ToString(Math.Round(df, 2) + " mm");
+
+                double h = 2 * m + c;
+                h_Ausgabe.Text = Convert.ToString(Math.Round(h, 2) + " mm");
+
+                double ha = m;
+                ha_Ausgabe.Text = Convert.ToString(Math.Round(ha, 2) + " mm");
+
+                double hf = m + c;
+                hf_Ausgabe.Text = Convert.ToString(Math.Round(hf, 2) + " mm");
+
+                double a = 10 * 10;
+                a_Ausgabe.Text = Convert.ToString(Math.Round(a, 2) + " mm");
+            }
+            else
+            {
+                if (z % 1 != 0)
+                {
+                    zähnezahl.Background = Brushes.Red;
+                    MessageBox.Show("Es gibt nur gerade Zähnezahlen :D ");
+                }
+                if(z <= 4)
+                {
+                    zähnezahl.Background = Brushes.Red;
+                    MessageBox.Show("Bitte mindestens eine Zähnzahl von 5 eingeben");
+                }
+                else { MessageBox.Show("Technischer Fehler, bitte wenden Sie sich an ihen Administrator"); }
+            }
         }
 
-        
+
     }
 }
